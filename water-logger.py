452c1450e4a55d961e26383916dc2b38cc15fca4
danny fabricant <dannyfabricant@users.noreply.github.com>
2016-05-09 21:58:51 -0400

    Update water-logger.py

--- water-logger.py
+++ water-logger.py
@@ -3,23 +3,35 @@ import json
 import sys
 import time
 from time import localtime
+from time import sleep
 import datetime
 import csv
 import os
 import sys
 import picam
 
-FREQUENCY_SECONDS      = 5
 
+# wait for time to be set to realtime
+sleep(5) 
+
+# period in between measurments
+FREQUENCY_SECONDS = 60 * 5
+
+# set pinmode
 GPIO.setmode(GPIO.BCM)
 
-# pins thge pi uses to communicate with the distance sensor
+# distance sensor pins
 TRIG = 23
 ECHO = 24
+DELAY = 60*10
+
+# shutdown button
+GPIO.setup(18,GPIO.IN)
 
 print 'Press Ctrl-C to quit.'
 
-def distance(): # a detailed description of how this function works is available in the modmypi tutorial
+# a detailed description of how this function works is available in the modmypi tutorial
+def distance(): 
 	GPIO.setup(TRIG,GPIO.OUT)
 	GPIO.setup(ECHO,GPIO.IN)
 	 
@@ -46,12 +58,15 @@ def distance(): # a detailed description of how this function works is available
 	GPIO.cleanup()
 
 def logMedian():
-	day = str(time.strftime("%d-%m-%Y"))
+	# set the current directory for the data to todays date
+	day = str(time.strftime("%m-%d-%Y"))
 	directory = '/media/pi/SANDISK/' + day
 	photodir = directory + "/photos"
 	filename = str(directory + "/" + day +'.csv')
 	print directory
 	
+	# check if directories exist
+	# create them if they do not
 	if os.path.exists(directory) == False:
 	    os.makedirs(directory)
 	    print "created directory"
@@ -59,24 +74,31 @@ def logMedian():
 	    os.makedirs(photodir)
 	    print "created directory"
 
+	# create an empty array to store the measurements
 	data = []
 	loop = 0
 	print 'beginning measurements'
-	while loop < 3:
+
+	# measure water level X amount of times 
+	while loop <= 8 :
 		m = distance()
 		data.append(m)
 		print "measured {0} times".format(loop)
 		loop = loop + 1
+
+	# sort the data by distance and grab the media value to help ensure accuracy
 	print data
 	data.sort()
-	median = str(data[1])
+	median = str(data[4])
 
+	# log the current time and measurment to a spreadsheet
 	with open(filename, 'a') as fp:
 	    writer = csv.writer(fp, dialect='excel', delimiter=',')
 	    timestamp = str(time.strftime("%m/%d, %H:%M"))
 	    writer.writerow([timestamp, median])
 	    print 'water logged'
 
+	# take a photo of the drain. Label it the row number of the corresponding measurement
 	row_count = 0
 	with open(filename, 'r') as f:
 		reader = csv.DictReader(f)
@@ -86,6 +108,17 @@ def logMedian():
 		photo = picam.takePhotoWithDetails(640,480, 85)
 		photo.save(photodir+"/"+row_count+".jpg")
 	
-	
 
-logMedian()
+# neverending loop	
+while True:
+	# check if the shutdown switch is on
+	button_current = GPIO.input(18)
+	if (button_current):
+		# shutdown the pi if it is
+		print "shutting down now"
+		os.system("sudo shutdown -h now")
+		sleep(1)
+
+	# call the function to log data
+	logMedian()
+	sleep(FREQUENCY_SECONDS)
